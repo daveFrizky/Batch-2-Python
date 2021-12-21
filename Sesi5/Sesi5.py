9b77c51a50f8f867d12e38bcd0a73a47824542a4
daveFrizky <frizkydavid@gmail.com>
2021-12-21 14:22:43 +0700

    up

--- Sesi5/Sesi5.py
+++ Sesi5/Sesi5.py
@@ -3,12 +3,40 @@ spock = ["Spock", 35, "Science Officer", 2254]
 mccoy = ["Leonard McCoy", "Chief Medical Officer", 2266]
 
 class Dog:
+    species = "Canis familiaris"
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-class Dog:
-     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    # Instance method
+    def description(self):
+        return f"{self.name} is {self.age} years old"
 
+    # Another instance method
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+heli=Dog("heli",1)
+print(heli.name," umur ",heli.age," tahun")
+
+class JackRussellTerrier(Dog):
+    pass
+
+class Dachshund(Dog):
+    pass
+
+class Bulldog(Dog):
+    pass
+
+
+buddy = Dachshund("Buddy", 9)
+jack = Bulldog("Jack", 3)
+jim = Bulldog("Jim", 5)
+
+class JackRussellTerrier(Dog):
+    def speak(self, sound="Arf"):
+        return f"{self.name} says {sound}"
+
+miles = JackRussellTerrier("Miles", 4)
+miles.speak()
