da6ff5841be8b83da28cae898987bb8b1b78b27a
daveFrizky <frizkydavid@gmail.com>
2021-12-21 09:33:49 +0700

    update

---
+++ Sesi5/Sesi5.py
@@ -0,0 +1,14 @@
+raka = ["Raka Ardhi", 28, "CurDev", 2265]
+spock = ["Spock", 35, "Science Officer", 2254]
+mccoy = ["Leonard McCoy", "Chief Medical Officer", 2266]
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Dog:
+     def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
