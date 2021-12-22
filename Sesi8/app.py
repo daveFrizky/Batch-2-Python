c004a1b0f96927da5bde944fe23cb33021739862
daveFrizky <frizkydavid@gmail.com>
2021-12-22 16:01:58 +0700

    up

--- Sesi8/app.py
+++ Sesi8/app.py
@@ -6,7 +6,7 @@ app = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
-
+app=app.app
 # Create a URL route in our application for "/"
 @app.route('/')
 def home():
